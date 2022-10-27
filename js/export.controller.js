668ee0f9b7bf953db744691e9feccf7f27a8c7de
Lior Atar <Liorat18@gmail.com>
2022-10-27 17:42:06 +0300

    added share to facebook

--- js/export.controller.js
+++ js/export.controller.js
@@ -1,8 +1,45 @@
 function downloadMeme() {
     gIsDownloadOn = true
-    // renderCanvas()
-    const imgContent = gElCanvas.toDataURL('image/jpeg')// image/jpeg the default format
-    document.querySelector('.download').href = imgContent
+    const imgDataUrl = gElCanvas.toDataURL('image/jpeg')// image/jpeg the default format
+    document.querySelector('.download').href = imgDataUrl
     gIsDownloadOn = false
-    // renderCanvas()
+}
+
+function uploadImg() {
+    const imgDataUrl = gElCanvas.toDataURL("image/jpeg")// Gets the canvas content as an image format
+
+    // A function to be called if request succeeds
+    function onSuccess(uploadedImgUrl) {
+        // Encode the instance of certain characters in the url
+        const encodedUploadedImgUrl = encodeURIComponent(uploadedImgUrl)
+        console.log(encodedUploadedImgUrl)
+        window.open(`https://www.facebook.com/sharer/sharer.php?u=${uploadedImgUrl}&t=${uploadedImgUrl}`)
+    }
+    // Send the image to the server
+    doUploadImg(imgDataUrl, onSuccess)
+}
+
+function doUploadImg(imgDataUrl, onSuccess) {
+    // Pack the image for delivery
+    const formData = new FormData()
+    formData.append('img', imgDataUrl)
+
+    // Send a post req with the image to the server
+    const XHR = new XMLHttpRequest()
+    XHR.onreadystatechange = () => {
+        // If the request is not done, we have no business here yet, so return
+        if (XHR.readyState !== XMLHttpRequest.DONE) return
+        // if the response is not ok, show an error
+        if (XHR.status !== 200) return console.error('Error uploading image')
+        const { responseText: url } = XHR
+        // If the response is ok, call the onSuccess callback function, 
+        // that will create the link to facebook using the url we got
+        console.log('Got back live url:', url)
+        onSuccess(url)
+    }
+    XHR.onerror = (req, ev) => {
+        console.error('Error connecting to server with request:', req, '\nGot response data:', ev)
+    }
+    XHR.open('POST', '//ca-upload.com/here/upload.php')
+    XHR.send(formData)
 }
